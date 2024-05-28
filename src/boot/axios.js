ef6e38536507d812ed2d981f7dec9328a7525fb4
Alexander Kugayevskii <daintars2@gmail.com>
2024-05-28 15:46:24 +0500

    add ip to axios

--- src/boot/axios.js
+++ src/boot/axios.js
@@ -14,6 +14,7 @@ import echo from "./chat";
 const TOKEN_LIFETIME = 2 * 60 * 60; // В секундах (2 часа)
 
 const url = "https://api.neoinsurance.uz";
+// const url = "http://127.0.0.1:8000";
 const api = axios.create({ baseURL: url + "/api" });
 
 export default boot(({ app, router }) => {
